42a79a75b495d986d90708d90de7fb6930ae24c9
bennymi <xbenmmusicx@gmail.com>
2023-01-24 20:25:03 +0100

    move header into separate file

--- src/lib/code/CodeBlock.svelte
+++ src/lib/code/CodeBlock.svelte
@@ -2,12 +2,11 @@
 	import hljs from 'highlight.js';
 
 	import { browser } from '$app/environment';
-	import { createEventDispatcher, onMount } from 'svelte';
+	import { onMount } from 'svelte';
 	import { writable, type Writable } from 'svelte/store';
 
 	import type { FocusBlock, UpdatedFocusBlock } from './types';
-
-	const dispatch = createEventDispatcher();
+	import Header from './Header.svelte';
 
 	// Props (functionality)
 	/** Set the Highlight.js language used for syntax highlighting. */
@@ -57,7 +56,6 @@
 	export let highlightColor: string = 'bg-gray-200/10';
 
 	// Local variables
-	let copyState = false;
 	// let hiddenCode: string = hljs.highlight(code, { language }).value.trim();
 	let hiddenCode: string = '';
 	let highlightedLinesList: number[] = [];
@@ -73,20 +71,6 @@
 				.getElementById(`svelte-code-line-${line}`)
 				?.scrollIntoView({ behavior: 'smooth', inline: 'center' });
 		}
-	};
-
-	const handleCopy = () => {
-		// Add code to clipboard
-		navigator.clipboard.writeText(String(code));
-
-		// Give feedback
-		copyState = true;
-		setTimeout(() => {
-			copyState = false;
-		}, 1500);
-
-		/** @event {{}} copy - Fire event when code is copied.  */
-		dispatch('copy', {});
 	};
 
 	// Animate a change in the focus block. Scroll to line and update focus.
@@ -241,34 +225,7 @@
 		<div class="code-block flex flex-col overflow-auto {classesCodeBlock}">
 			<!-- Header -->
 			{#if showHeader}
-				<header
-					class="code-block-header rounded-t-lg flex justify-between items-center p-2 pl-4 {classesHeader}"
-				>
-					<!-- Language Text -->
-					<span class="code-block-language">{headerText}</span>
-
-					<!-- Copy Button -->
-					<button
-						class="code-block-btn px-2 py-1 rounded-lg transition-all duration-200 text-white/70 hover:text-white/95"
-						on:click={handleCopy}
-					>
-						{#if copyState}
-							<svg xmlns="http://www.w3.org/2000/svg" class="w-5" viewBox="0 0 24 24"
-								><path
-									fill="currentColor"
-									d="M9 18q-.825 0-1.412-.587Q7 16.825 7 16V4q0-.825.588-1.413Q8.175 2 9 2h9q.825 0 1.413.587Q20 3.175 20 4v12q0 .825-.587 1.413Q18.825 18 18 18Zm-4 4q-.825 0-1.413-.587Q3 20.825 3 20V7q0-.425.288-.713Q3.575 6 4 6t.713.287Q5 6.575 5 7v13h10q.425 0 .713.288q.287.287.287.712t-.287.712Q15.425 22 15 22Z"
-								/></svg
-							>
-						{:else}
-							<svg xmlns="http://www.w3.org/2000/svg" class="w-5" viewBox="0 0 24 24"
-								><path
-									fill="currentColor"
-									d="M9 18q-.825 0-1.412-.587Q7 16.825 7 16V4q0-.825.588-1.413Q8.175 2 9 2h9q.825 0 1.413.587Q20 3.175 20 4v12q0 .825-.587 1.413Q18.825 18 18 18Zm0-2h9V4H9v12Zm-4 6q-.825 0-1.413-.587Q3 20.825 3 20V7q0-.425.288-.713Q3.575 6 4 6t.713.287Q5 6.575 5 7v13h10q.425 0 .713.288q.287.287.287.712t-.287.712Q15.425 22 15 22ZM9 4v12V4Z"
-								/></svg
-							>
-						{/if}
-					</button>
-				</header>
+				<Header {classesHeader} {headerText} {code} on:copy />
 			{/if}
 
 			<!-- This element is hidden. We are only using it to bind it to a variable which has the correct childNodes of the code that should be displayed.
