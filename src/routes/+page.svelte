a34c4828d8bd6e138fb485c1b4e63014d48886e9
bennymi <xbenmmusicx@gmail.com>
2023-02-11 12:09:19 +0100

    add neon text shadow and update intro slide

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,22 @@
 	import { addStepAnimation } from '$lib/addstepanimation';
 </script>
 
-<div class="flex h-screen max-h-screen flex-col justify-center">
+<div class="flex h-screen max-h-screen flex-col justify-center bg-gray-900">
 	<div class="max-w-5/6 mx-auto ">
 		<div class="animate__animated step-1:animate__flip" use:addStepAnimation>
 			<iconify-icon icon="openmoji:deciduous-tree" class="text-9xl" />
 		</div>
 	</div>
 	<h1
-		class="max-w-5/6 mx-10 my-2 text-center font-sans text-5xl font-black leading-none tracking-tighter text-neutral-900"
+		class="max-w-5/6 mx-10 my-2 text-center font-sans text-5xl font-black leading-none tracking-tighter text-white"
 	>
-		Welcome to svelte-presentation
+		Welcome to <span class="text-[#fff6a9] text-shadow-neon">SvelteShow</span>
 	</h1>
 	<div
-		class="max-w-4/6 mx-auto mt-8 text-center font-sans text-2xl font-normal leading-relaxed text-gray-600"
+		class="max-w-4/6 mx-auto mt-8 text-center font-sans text-2xl font-normal leading-relaxed text-gray-400"
 	>
 		<p>
-			A tool for creating presentations using <iconify-icon
+			A tool for creating presentations and slideshows using <iconify-icon
 				icon="logos:svelte-kit"
 				class="text-2xl"
 			/>
@@ -28,21 +28,21 @@
 	<nav class="mx-auto mt-8 flex space-x-4">
 		<a
 			href="https://github.com/bennymi/svelte-presentation"
-			class="flex items-center justify-center gap-1 font-sans text-lg font-semibold leading-relaxed text-blue-600 hover:text-blue-700"
+			class="flex items-center justify-center gap-1 font-sans text-lg font-semibold leading-relaxed text-blue-500 hover:text-blue-600"
 		>
 			<iconify-icon icon="mdi:github" class="text-2xl" />
 			GitHub
 		</a>
 		<a
 			href={'/'}
-			class="flex items-center justify-center gap-1 font-sans text-lg font-semibold leading-relaxed text-gray-800 hover:text-gray-900"
+			class="flex items-center justify-center gap-1 font-sans text-lg font-semibold leading-relaxed text-gray-300 hover:text-gray-400"
 		>
 			<iconify-icon icon="mdi:book-open-page-variant" class="text-2xl" />
 			Documentation
 		</a>
 		<a
 			href={'/'}
-			class="flex items-center justify-center gap-1 font-sans text-lg font-semibold leading-relaxed text-orange-600 hover:text-orange-700"
+			class="flex items-center justify-center gap-1 font-sans text-lg font-semibold leading-relaxed text-orange-500 hover:text-orange-600"
 		>
 			<iconify-icon icon="mdi:presentation-play" class="text-2xl" />
 			Example
