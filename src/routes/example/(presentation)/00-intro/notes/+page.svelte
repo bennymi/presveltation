53ef87085cef781a40429fdb3b6d7cd457b44ce5
bennymi <xbenmmusicx@gmail.com>
2023-02-12 16:37:34 +0100

    update intro slide with info on how to open overview and notes, and update notes

--- src/routes/example/(presentation)/00-intro/notes/+page.svelte
+++ src/routes/example/(presentation)/00-intro/notes/+page.svelte
@@ -1,13 +1,20 @@
 <div class="flex h-screen flex-col items-center justify-center">
 	<h1 class="mb-8 text-7xl font-bold">Example notes</h1>
-	<ul class="mx-8 list-disc">
-		<li>Notes have a timer at the top left.</li>
+	<ul class="mx-4 list-inside list-disc">
+		<li>These notes auto-update when the presentation slides move forward.</li>
+		<li class="ml-8">
+			Arrow keys are not recognized in the notes window, so focus on the window where the slides are
+			shown to update slides and notes.
+		</li>
+		<li>Notes have a timer at the top left, so that you can track your time.</li>
 		<li>
-			Notes are displayed through an iframe, and the <span
-				class="rounded-lg bg-blue-500 p-1 text-white">+page.svelte</span
+			To create a note you simply create a new subdirectory in the slide directory for which you
+			want to create notes:
+		</li>
+		<li class="ml-8">
+			<span class="rounded-lg bg-blue-500 p-1 text-white"
+				>src/routes/(slides)/00-introduction/notes/+page.svelte</span
 			>
-			note is placed into a <span class="rounded-lg bg-blue-500 p-1 text-white">/notes</span> sub-folder
-			within the slide directory.
 		</li>
 		<li>The next slides show what notes look like if there were no notes added.</li>
 	</ul>
