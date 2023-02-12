c25eda1ebba6e71c7c779ba66650206368a8eb31
bennymi <xbenmmusicx@gmail.com>
2023-02-12 15:52:56 +0100

    add timer component and add it to the Speaker so it shows up in the notes

--- src/lib/Speaker.svelte
+++ src/lib/Speaker.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import { slides, currSlide, currStep, maxSteps } from '$lib/stores';
+	import Timer from './Timer.svelte';
 
 	$: console.log('Inside Notes: currStep =', $currStep);
 	$: slide = $slides[$currSlide];
 	$: notesRoute = slide.notes ? slide.notesRoute ?? `${slide.route}/notes` : '';
 	$: console.log('Inside Notes: slide =', slide);
 </script>
+
+<div class="fixed top-0 left-0 z-20 m-4">
+	<Timer />
+</div>
 
 {#if notesRoute}
 	<iframe
@@ -16,13 +21,13 @@
 	/>
 {:else}
 	<div class="flex h-screen min-h-screen flex-col items-center justify-center">
-		<h1 class="text-center text-2xl font-bold">
-			Slide {$currSlide + 1}:
-			<span class="text-blue-700">
-				{slide.title}
-			</span>
+		<h1 class="text-center text-xl font-bold">
+			Slide {$currSlide + 1}/{$slides.length}
 		</h1>
-		<p class="mt-2 text-gray-700">No Notes Available</p>
+		<h1 class="mt-2 mb-4 text-center text-2xl font-bold text-blue-700">
+			{slide.title}
+		</h1>
+		<p class="text-gray-700">No Notes Available</p>
 		<iframe
 			src={slide.route}
 			class="h-3/4 w-3/4 scale-75 border-2 border-blue-700"
