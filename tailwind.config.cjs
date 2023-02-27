068ba96e1c44f5724fdd8fb08ace57040999469d
bennymi <xbenmmusicx@gmail.com>
2023-02-27 19:50:02 +0100

    update intro slide

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,8 @@ module.exports = {
         DEFAULT: '0 2px 4px var(--tw-shadow-color)',
         lg: '0 8px 16px var(--tw-shadow-color)',
         // https://codepen.io/ananyaneogi/pen/Bgozrz
-        neon: '0 0 5px #ffa500, 0 0 15px #ffa500, 0 0 20px #ffa500, 0 0 40px #ffa500, 0 0 60px #ff0000, 0 0 10px #ff8d00, 0 0 98px #ff0000'
+        // neon: '0 0 5px #ffa500, 0 0 15px #ffa500, 0 0 20px #ffa500, 0 0 40px #ffa500, 0 0 60px #ff0000, 0 0 10px #ff8d00, 0 0 98px #ff0000'
+        neon: '0 0 5px #ffa500, 0 0 7px #ffa500, 0 0 10px #ffa500, 0 0 14px #ffa500, 0 0 18px #ff0000, 0 0 9px #ff8d00, 0 0 25px #ff0000'
       },
     },
   },
