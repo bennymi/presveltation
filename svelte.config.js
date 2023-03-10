3f4960f54e8a59c8250ea6de80fdbd3416d83b4b
Michael Riley <michaelriley@situsamc.com>
2023-03-09 17:07:09 -0700

    added mdsvex, typography, remark-math, rehype-katex

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,14 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions:[".svelte",".svx",".md"],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(),
+		mdsvex({ extensions: ['.svx', '.md'],
+
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatexSvelte] })],
 
 	kit: {
 		adapter: adapter()
